218377af23202a1969bae44448470655aed3e597
Muhammad Areeb <areeb.m2001@gmail.com>
2023-11-02 15:57:55 +0500

    fixed css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,79 +23,38 @@ export default function Home() {
         </Box>
         <Box className={classes.buttonyearly}>
           <Button
-            style={{
-              height: "35px",
-              backgroundColor: "white",
-              color: "#939393",
-              borderRadius: "4px",
-              border: "1px black",
-            }}
+            className={ classes.wrapyearlybutton}
             endIcon={<KeyboardArrowDownIcon />}
           >
             Yearly
           </Button>
         </Box>
       </Box>
-      <Box style={{ display: "flex" }} marginTop={1}>
+      <Box className={classes.wrapdashboardsummary}marginTop={1}>
         <DashboardSummary />
       </Box>
       <Box 
       className={classes.mainbox}
-        // style={{
-        //   display: "flex",
-        //   flexDirection: "row",
-        //   marginLeft: "200px",
-        //   width: "81%",
-        //   marginBottom:'20px'
-        // }}
       >
         <Box 
         className={classes.boxa}
-          // style={{
-          //   display: "flex",
-          //   height: "500px",
-          //   width: "76%",
-          //   flexDirection: "column",
-          // }}
         >
           <Box
-            style={{
-              backgroundColor: "white",
-              height: "240px",
-              marginBottom: "20px",
-              width: "100%",
-              borderRadius: "5px",
-              boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)",
-            }}
-          ></Box>
+           className={classes.boxa1}
+          >
+          </Box>
           <Box
-            style={{
-              backgroundColor: "white",
-              height: "240px",
-              borderRadius: "5px",
-              width: "100%",
-              boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)",
-            }}
+            className={classes.boxa1}
           ></Box>
         </Box>
         <Box 
         className={classes.boxb}
-          // style={{
-            
-            // display:'flex',
-            // height: "500px",
-            // width: "20%",
-            // flexDirection: "column",
-            // marginLeft: "20px",
-            // gap:'30px',
-            // marginBottom:'10px'
-          // }}
         >
-          <Box style={{  height: "150px",backgroundColor: "white",boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)", borderRadius: "5px",}}>
+          <Box className={classes.boxb1}>
           </Box>
-          <Box style={{  height: "150px",backgroundColor: "white",boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)",  borderRadius: "5px",}}>
+          <Box className={classes.boxb1}>
           </Box>
-          <Box style={{  height: "150px",backgroundColor: "white",boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)", borderRadius: "5px",}}>
+          <Box className={classes.boxb1}>
           </Box>
         </Box>
       </Box>
--- src/app/stylesheet.ts
+++ src/app/stylesheet.ts
@@ -1,84 +1,105 @@
 import { makeStyles } from "@mui/styles";
 export default makeStyles({
-    mainbox:{
+    mainbox: {
         display: "flex",
         flexDirection: "row",
         marginLeft: "200px",
         width: "85.5%",
-        marginBottom:'20px',
-        '@media (max-width:1100px)':{
-            width:'80%',
-            flexDirection:'column'
+        marginBottom: '20px',
+        '@media (max-width:1100px)': {
+            width: '80%',
+            flexDirection: 'column'
         },
-        '@media (max-width:650px)':{
-            width:'70%',
-            flexDirection:'column'
+        '@media (max-width:650px)': {
+            width: '70%',
+            flexDirection: 'column'
         },
-        '@media (max-width:400px)':{
-            width:'70%',
-            flexDirection:'column'
+        '@media (max-width:400px)': {
+            width: '70%',
+            flexDirection: 'column'
         }
     },
-    boxa:{
+    boxa: {
         display: "flex",
         height: "500px",
         width: "76%",
         flexDirection: "column",
-        marginTop:'10px',
-        '@media (max-width:1100px)':{
-            width:'98%',
-            flexDirection:'column'
+        marginTop: '10px',
+        '@media (max-width:1100px)': {
+            width: '98%',
+            flexDirection: 'column'
         },
-        '@media (max-width:650px)':{
-            width:'90%',
-            flexDirection:'column'
+        '@media (max-width:650px)': {
+            width: '90%',
+            flexDirection: 'column'
         }
     },
-    boxb:{
-        display:'flex',
+    boxa1: {
+        backgroundColor: "white",
+        height: "240px",
+        marginBottom: "20px",
+        width: "100%",
+        borderRadius: "5px",
+        boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)",
+    },
+    boxb: {
+        display: 'flex',
         height: "500px",
         width: "20%",
         flexDirection: "column",
         marginLeft: "20px",
-        gap:'30px',
-        marginTop:'10px',
-        marginBottom:'10px',
-        '@media (max-width:1100px)':{
-            width:'50%',
-            flexDirection:'column',
+        gap: '30px',
+        marginTop: '10px',
+        marginBottom: '10px',
+        '@media (max-width:1100px)': {
+            width: '50%',
+            flexDirection: 'column',
             marginLeft: "0px",
         },
-        '@media (max-width:650px)':{
-            width:'80%',
-            flexDirection:'column',
+        '@media (max-width:650px)': {
+            width: '80%',
+            flexDirection: 'column',
             marginLeft: "0px",
         }
 
     },
-    details:{
+    boxb1: {
+        height: "150px",
+        backgroundColor: "white",
+        boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.2)",
+        borderRadius: "5px"
+    },
+    details: {
         display: "flex",
-         marginLeft: "200px",
-          marginTop: "80px",
-          '@media (max-width:650px)':{
-            width:'30%',
-            flexDirection:'column',
-            marginleft:'20px',
-            // marginLeft: "0px",
+        marginLeft: "200px",
+        marginTop: "80px",
+        '@media (max-width:650px)': {
+            width: '30%',
+            flexDirection: 'column',
+            marginleft: '20px',
         }
     },
-    buttonyearly:{
+    buttonyearly: {
         display: "flex",
-         marginLeft: "60%",
-         '@media (max-width:650px)':{
-            width:'100%',
-            flexDirection:'column',
-            // marginLeft: "0px",
+        marginLeft: "60%",
+        '@media (max-width:650px)': {
+            width: '100%',
+            flexDirection: 'column',
             marginLeft: "0%",
         },
-        '@media (max-width:400px)':{
-            width:'100%',
-            flexDirection:'column',
-            // marginLeft: "0px",
+        '@media (max-width:400px)': {
+            width: '100%',
+            flexDirection: 'column',
         }
+    },
+    wrapdashboardsummary: {
+        display: 'flex'
+    },
+    wrapyearlybutton: {
+        height: "35px",
+        backgroundColor: "white",
+        color: "#939393",
+        borderRadius: "4px",
+        border: "1px black",
     }
 })
